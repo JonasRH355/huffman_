bc1bdba69ceeb3175152fa0cbad38ad6709aa404
Booxmann - Jonas <jonatas.hellmann355@gmail.com>
2024-05-26 22:55:14 -0300

    all

--- huffmantree.h
+++ huffmantree.h
@@ -31,197 +31,27 @@ struct Arvore{
 };
 
 struct MapadeBits{
-
+    char caracter;
+    std::string bits;
+    MapadeBits *next;
 };
 
-//_____________________Funções padrões__________________________
-void mostrarArvore(NoArvore *raiz){
-    if (raiz == nullptr) return;
-    mostrarArvore(raiz->esquerdo);
-    std::cout<<raiz->caracter<<" : "<<raiz->frequencia<<std::endl;
-    mostrarArvore(raiz->direito);
-}
-
-
-void mostrarLista (lista text){ //mostra a lista
-    No *aux = text.inicio;
-    while (aux != nullptr) {
-        std::cout << aux->letter<< " : "<< aux->frequencia<< "\n";
-        aux = aux->direito;
-    }
-}
-
-bool inserirlista(lista &lista,char caracter,int freq){ // iserir de forma ordenada
-    auto *novo = new No;
-    novo->letter = caracter;
-    novo->frequencia = freq;
-    if (novo->frequencia <= lista.inicio->frequencia) {      //caso for igual ou menor que a primeira
-        novo->direito = lista.inicio;
-        lista.inicio = novo;
-        return true;
-    } else if (novo->frequencia > lista.fim->frequencia) {
-        lista.fim->direito = novo;
-        lista.fim = novo;
-        return true;
-    }                                    //caso for a maior
-    else {
-        No *aux = lista.inicio;
-        while (aux != nullptr) {  //caso 1: aux <= x <= next
-            if (aux->frequencia <= novo->frequencia && novo->frequencia <= aux->direito->frequencia) {
-                novo->direito = aux->direito;
-                aux->direito = novo;
-                return true;
-            }
-            aux = aux->direito;
-        }
-        return false;
-    }
-}
-
-bool inserirnacopia(lista &lista,No ){
-
-}
-
-bool retirarLista(lista &lista, char valor ){ // Função feita para retirar da lista
-    if( lista.inicio == nullptr ) return false;
-
-    No *aux = lista.inicio;
-    No *ant = nullptr;
-    while( aux != nullptr && valor != aux->letter ){ // loop para procurar o no da lista
-        ant = aux;
-        aux = aux->direito;
-    }
-    if( aux == nullptr ) return false; // caso não esteja na lista
-
-    if( aux == lista.inicio && aux == lista.fim ){ // Se for o único da lista
-        lista.inicio = nullptr;
-        lista.fim = nullptr;
-        delete aux;
-        return true;
-    }
-    if( aux == lista.inicio ){ // Se for o primeiro da Lista
-        lista.inicio = aux->direito;
-        delete aux;
-        return true;
-    }
-    if( aux == lista.fim ){ // Se for ultimo da lista
-        ant->direito = nullptr;
-        lista.fim = ant;
-        delete aux;
-        return true;
-    }
-    // se estiver no meio da lista
-    ant->direito = aux->direito;
-    delete aux;
-    return true;
-}
-//_____________________Fim Funções Padrões__________________________________
-
-//______________________Funções gerais______________________________________
-bool inserirlistadefrequencia(lista &x,char caracter ){
-    if (x.inicio == nullptr && x.fim == nullptr){  // Caso que acontece quando não tem nada na lista
-        No* novoNo = new No;
-        novoNo->letter = caracter;
-        novoNo->frequencia = 1;
-        x.inicio= novoNo;
-        x.fim= novoNo;
-        return  true;
-    }
-
-    No *current = x.inicio;
-    while (current) { // loop que percorre a lista...
-        if (current->letter == caracter) {   // caso já tenha na lista,
-            int freq = current->frequencia +1;  // ele aumenta +1 de frequência e realoca dentro da lista
-            retirarLista(x,current->letter);
-            inserirlista(x,caracter,freq);
-            return true;
-        }
-        if (!current->direito){ // se chegou até o ultimo da lista e não o achou, inseri ele no começo dela
-            inserirlista(x,caracter,1);
-            return true;
-        }
-        current = current->direito;
-    }
-
-    return true;
-}
-
-bool listadefrequencia(const char text,No*  &x){ // Função que gera a lista de frequência
-    No* atual = x;
+bool listadefrequencia(const char text,No*  &x) { // Função que gera a lista de frequência
+    No *atual = x;
     while (atual != nullptr && atual->letter != text) {
         atual = atual->direito;
     }
     if (atual != nullptr) {
         atual->frequencia++;
     } else {
-        No* novafrequencia = new No();
+        No *novafrequencia = new No();
         novafrequencia->letter = text;
         novafrequencia->frequencia = 1;
         novafrequencia->direito = x;
         x = novafrequencia;
     }
 }
-//_____________________Fim Funções Gerais_____________________________________
 
-
-//_________________________Árvore_____________________________________________
-
-int somaFrequencia (int x,int y){
-    return x+y;
-}
-
-void copieNos(No *x, No *&y){
-    y->frequencia = x->frequencia;
-    y->letter = x->letter;
-}
-/*
-bool brotoDaLista(lista x,Arvore &z)  // Função que cria um árvore dos primeiros nós da lista
-{
-    No *aux = x.inicio;
-    auto *raiz = new NoArvore('c',3);
-    auto *esq = new No;
-    auto *dir = new No;
-    int soma = somaFrequencia(aux->frequencia,aux->direito->frequencia);
-    raiz->frequencia= soma;
-    copieNos(aux,dir);
-    copieNos(aux->direito,esq);
-    raiz->direito = dir;
-    raiz->esquerdo = esq;
-    z.raiz = raiz;
-    return true;
-}
-*/
-bool criarArvore(lista l_ordenada, Arvore ArvoreMae){
-    if(l_ordenada.inicio == nullptr){return false;}
-    No *aux = l_ordenada.inicio;
-    lista copia = l_ordenada;
-    /*
-    while (aux){
-        if(aux->direito->frequencia == ArvoreMae.raiz->frequencia)
-        {
-            if (aux->direito->direito->frequencia >= ArvoreMae.raiz->frequencia){
-                brotoDaLista(copia,ArvoreMae);
-            }
-            if (aux->direito->direito->frequencia == ArvoreMae.raiz->frequencia){
-
-            }
-        }
-        aux = aux->direito;
-    }*/
-/*
-    while(aux){
-
-
-        brotoDaLista(copia,ArvoreMae);
-        retirarLista(copia,aux->letter);
-
-        aux = aux->direito;
-    }
-
-
-    mostrarArvore(ArvoreMae.raiz);*/
-}
-//_______________________Fim Árvore___________________________________________
 bool inserir(NoArvore * &raiz, char caracter, unsigned freq){
     if(raiz == nullptr){
         raiz = new NoArvore(caracter,freq);
@@ -234,16 +64,53 @@ bool inserir(NoArvore * &raiz, char caracter, unsigned freq){
     }
 }
 
+void inserirNoLista(Lista* &topo, NoArvore* no){
+    Lista* newNode = new Lista();
+    newNode->nodaArvore = no;
+    newNode->proximo = nullptr;
+
+    if (topo == nullptr || topo->nodaArvore->frequencia >= no->frequencia) {
+        newNode->proximo = topo;
+        topo = newNode;
+    } else {
+        Lista* Atual = topo;
+        while (Atual->proximo != nullptr && Atual->proximo->nodaArvore->frequencia < no->frequencia) {
+            Atual = Atual->proximo;
+        }
+        newNode->proximo = Atual->proximo;
+        Atual->proximo = newNode;
+    }
+}
+
+void atualizarbitmapa(MapadeBits* &topo, char caracter,std::string bits){
+    MapadeBits* novobit = new MapadeBits();
+    novobit->caracter = caracter;
+    novobit->bits = bits;
+    novobit->next = topo;
+    topo = novobit;
+}
+
+void mostrarHuffman(NoArvore* rota,std::string str,MapadeBits* &bitmap){
+    if (!rota)
+        return;
+
+    if (rota->caracter != '$')
+        atualizarbitmapa(bitmap, rota->caracter, str);
+
+    mostrarHuffman(rota->esquerdo, str + "0", bitmap);
+    mostrarHuffman(rota->direito, str + "1", bitmap);
+}
+
 
 void codigo_de_huffman(std::string text){
     No * lista_defrequencia = nullptr;
     for (char i : text) {
-        listadefrequencia(text[i],lista_defrequencia);
+        listadefrequencia(i,lista_defrequencia);
     }
     Lista* listaNo = nullptr;
     No* atual = lista_defrequencia;
     while (atual != nullptr) {
-        inserirNoLista(listaNo, new No(atual->letter, atual->frequencia));
+        inserirNoLista(listaNo, reinterpret_cast<NoArvore *>(new No(atual->letter, atual->frequencia)));
         atual = atual->direito;
     }
 
@@ -260,12 +127,12 @@ void codigo_de_huffman(std::string text){
     }
 
     MapadeBits* bitMap = nullptr;
-    printHuffmanCodes(listaNo->nodaArvore, "", bitMap);
+    mostrarHuffman(listaNo->nodaArvore, "", bitMap);
 
     // Imprimir tabela de bits
     std::cout << "Tabela de Bits:\n";
     while (bitMap != nullptr) {
-        std::cout << bitMap->character << " : " << bitMap->bits << "\n";
+        std::cout << bitMap->caracter << " : " << bitMap->bits << "\n";
         bitMap = bitMap->next;
     }
 }
--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "huffmantree.h"
+#include <string>
+
 
 int main() {
     lista listadefrequenci;
@@ -8,11 +10,9 @@ int main() {
     //Primeira parte: pegar um text e criar uma lista
     // em ordem crescente com caracteres e sua frequência
     text = "My name is Jonatas"; // a=3 s=2 n=2 m=2
-    ;
-    mostrarLista(listadefrequenci);
 
     // Segunda parte: Codigo de Huffman
-    codigo_de_huffman(listadefrequenci);
+    codigo_de_huffman(text);
 
     return 0;
 }
